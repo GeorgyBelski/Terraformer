521f4aa21712d5ef381d6e6c16ed7a55cd12c6bc
GeorgyBelski <45123704+GeorgyBelski@users.noreply.github.com>
2019-12-13 01:14:21 +0100

    Start adding of SingleTarget ability

--- Assets/Scripts/TowerAbilities/AbilityButtonController.cs
+++ Assets/Scripts/TowerAbilities/AbilityButtonController.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 public abstract class AbilityButtonController : MonoBehaviour
 {
     public enum State { Ready, Aiming, Casting, Recharging };
+    public enum Type { Area, SingleTarget};
     public KeyCode key;
     public TowerType castTowerType;
     public State currentState = State.Ready;
- //   public float castTime = 2.0f;
- //   public float timerCast;
+    public Type type;
+    //   public float castTime = 2.0f;
+    //   public float timerCast;
     public float coolDown = 5.0f;
     public int cost = 10;
     public float timerCoolDown;
@@ -28,17 +30,22 @@ public abstract class AbilityButtonController : MonoBehaviour
 
     Tower casterTower;
     Tower previousHighlightedTower;
+    Enemy target;
     float camRayLength = 90f;
     int groundMask;
-    Vector3 mousePos;
+    int enemyMask;
+    int enemy_groundMask;
+  //  Vector3 mousePos;
 
     Image buttonImage, outLineImage;
     Button button;
 
     void Start()
     {
-        groundMask = LayerMask.GetMask("Ground");
-        if(!buttonImage) buttonImage = GetComponent<Image>();
+        groundMask = Globals.groundLayerMask;
+        enemyMask = EnemyManagerPro.enemyLayerMask;
+        enemy_groundMask = enemyMask | groundMask;
+        if (!buttonImage) buttonImage = GetComponent<Image>();
         button = GetComponent<Button>();
         parent = transform.parent.gameObject;
         outLineImage = parent.GetComponent<Image>();
@@ -77,7 +84,10 @@ public abstract class AbilityButtonController : MonoBehaviour
                         casterTower.isHighlighted = false;
                         previousHighlightedTower = null;
                         ///   casterTower.CastThanderBall(aimArea.position);
-                        TowerCastAreaAbility(casterTower);
+                        if (type == Type.Area)
+                        { TowerCastAreaAbility(casterTower); }
+                        else 
+                        { TowerCastSingleTargetAbility(casterTower, target); }
                         outLineImage.enabled = false;
                         aimingAbility = null;
                     }
@@ -103,9 +113,10 @@ public abstract class AbilityButtonController : MonoBehaviour
 
         }
     }
-    public abstract void TowerCastAreaAbility(Tower casterTower);/// <summary>
-                                                                 /// casterTower.CastThanderBall(aimArea.position);  // example for ElectroTower
-                                                                 /// </summary>
+    public virtual void TowerCastAreaAbility(Tower casterTower) { }/// <summary>
+                                                                   /// casterTower.CastThanderBall(aimArea.position);  // example for ElectroTower
+                                                                   /// </summary>
+    public virtual void TowerCastSingleTargetAbility(Tower casterTower, Enemy target) { }
 
     public void Cancel() {       
         currentState = State.Ready;
@@ -120,7 +131,9 @@ public abstract class AbilityButtonController : MonoBehaviour
     }
     public void Activate()
     {
-    //    timerCast = castTime;
+        //    timerCast = castTime;
+        if (button.interactable == false)
+        { return; }
 
         if (currentState == State.Ready)
         {
@@ -147,7 +160,7 @@ public abstract class AbilityButtonController : MonoBehaviour
         }
         else
         {
-            aimAreaAnimator.SetBool("isSetted", false);
+            aimAreaAnimator.SetBool("isVanising", false);
         }
     }
 
@@ -157,38 +170,50 @@ public abstract class AbilityButtonController : MonoBehaviour
         {
             ResourceManager.DisplayCost(true, cost);
             Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(camRay, out RaycastHit floorHit, camRayLength, groundMask))
+            
+            if (type == Type.Area && Physics.Raycast(camRay, out RaycastHit floorHit, camRayLength, groundMask))
             {
-                mousePos = floorHit.point;
+                Debug.DrawLine(Camera.main.transform.position, floorHit.point, Color.cyan);
                 aimArea.position = floorHit.point;
-            //    TowerManager.ClearHighlighting();
-                Tower nearestTower = TowerManager.GetNearestTower(aimArea, castTowerType);
-                if (nearestTower == null)
-                { Cancel(); }
-                else if (nearestTower == previousHighlightedTower)
-                { return; }
-                if (castTowerType == TowerType.Electro)
-                {
-                    casterTower = (ElectroTower)nearestTower;
-                }
-                else if (castTowerType == TowerType.Laser)
-                {
-                    casterTower = (LaserTower)nearestTower;
-                }
-                else if (castTowerType == TowerType.Plasma)
-                {
-                    casterTower = (PlasmaTower)nearestTower;
-                }
-
-                if (casterTower && casterTower != previousHighlightedTower)
-                {
-                    casterTower.isHighlighted = true;
-                    if (previousHighlightedTower) { previousHighlightedTower.isHighlighted = false; }
-                    previousHighlightedTower = casterTower;
-                }
-                
-
+                DefineCasterTower();
             }
+            else if (type == Type.SingleTarget && Physics.Raycast(camRay, out RaycastHit enemyHit, camRayLength, enemy_groundMask))
+            {
+                Debug.DrawLine(Camera.main.transform.position, enemyHit.point, Color.yellow);
+                aimArea.position = enemyHit.collider.gameObject.transform.position;
+                EnemyManagerPro.TransformEnemyMap.TryGetValue(enemyHit.collider.transform, out target);
+                if(target) Debug.DrawLine(Camera.main.transform.position, enemyHit.point, Color.yellow);
+                else Debug.DrawLine(Camera.main.transform.position, enemyHit.point, Color.white);
+                Debug.Log(target);
+                DefineCasterTower();
+            }
+        }
+    }
+    void DefineCasterTower() 
+    {
+        Tower nearestTower = TowerManager.GetNearestTower(aimArea, castTowerType);
+        if (nearestTower == null)
+        { Cancel(); }
+        else if (nearestTower == previousHighlightedTower)
+        { return; }
+        if (castTowerType == TowerType.Electro)
+        {
+            casterTower = (ElectroTower)nearestTower;
+        }
+        else if (castTowerType == TowerType.Laser)
+        {
+            casterTower = (LaserTower)nearestTower;
+        }
+        else if (castTowerType == TowerType.Plasma)
+        {
+            casterTower = (PlasmaTower)nearestTower;
+        }
+
+        if (casterTower && casterTower != previousHighlightedTower)
+        {
+            casterTower.isHighlighted = true;
+            if (previousHighlightedTower) { previousHighlightedTower.isHighlighted = false; }
+            previousHighlightedTower = casterTower;
         }
     }
 
@@ -231,7 +256,7 @@ public abstract class AbilityButtonController : MonoBehaviour
         {
             //    Destroy(aimArea.gameObject);
             //   aimArea = null;
-            aimAreaAnimator.SetBool("isSetted", true);
+            aimAreaAnimator.SetBool("isVanising", true);
         }
     }
 
---
+++ Assets/Scripts/TowerAbilities/DeathBeamAbility.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBeamAbility : TowerAbility
+{ 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public override void CastingControl()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ShootingControl()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+}
